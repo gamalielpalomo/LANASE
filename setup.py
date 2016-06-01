1c59d050fd5b480142f6005c020cec58dc7984ec
gamalielpalomo <gama.abi_@hotmail.com>
2016-06-01 12:20:43 -0500

    Delete setup.py

--- setup.py
+++
@@ -1,3 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(name =  "LANASE GridFTP File Transfer", version = "1.1.2", description = "LANASE GridFTP File Transfer", executables = [Executable("grid.py")] , )
